1d19eea7d866a804615f4f77e39b1e5de30714d7
Thomas Faabry <airtonfabre95@gmail.com>
2023-03-01 13:13:57 -0300

    Update snake.py

--- day 20/snake.py
+++ day 20/snake.py
@@ -34,6 +34,7 @@ class Snake:
         self.add_segment(self.segments[-1].position())
 
 
+    #-------------------- TODO 2:Move the snake --------------------
     def move(self):
         for seg_num in range(len(self.segments) -1, 0, -1):
                 x_cor = self.segments[seg_num - 1].xcor()
@@ -63,30 +64,5 @@ class Snake:
             self.segments[0].setheading(RIGHT)
 
 
-            #-------------------- TODO 2:Move the snake --------------------
+
             
-
-
-
-# class Snake:
-#     def __init__(self):
-#         self.segments = []
-#         self.create_snake()
-#         self.head = self.segments[0]
-
-
-#     def create_snake(self):
-#         for position in STARTING_POSITIONS: 
-#             new_segment = Turtle(shape="square")
-#             new_segment.color("white")
-#             new_segment.penup()
-#             new_segment.goto(position)
-#             self.segments.append(new_segment)
-
-
-#     def move(self):
-#         for seg_num in range(len(self.segments) -1, 0, -1):
-#             x_coordinate = self.segments[seg_num -1].xcor()
-#             y_coordinate = self.segments[seg_num -1].ycor()
-#             self.segments[seg_num].goto(x_coordinate, y_coordinate)
-#         self.head.forward(MOVE_DISTANCE)
